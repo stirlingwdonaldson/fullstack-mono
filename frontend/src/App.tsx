3b9db795482b8fc607a28aa803cadddb2c1bcf8e
Stirling Donaldson <s4169686@student.rmit.edu.au>
2026-04-23 15:51:40 +1000

    style(app): refactored app, looks nice

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import Nav from "@/components/nav"
-
-
+import bg from "@/assets/bg.jpg"
 
 function App() {
-
   return (
-    <div className="w-full flex flex-col justify-center items-center align-middle">
-      <Nav />
+    <div className="min-h-screen">
+      <section className="relative h-screen overflow-hidden">
+
+        <div
+          className="absolute inset-0 bg-center bg-cover bg-no-repeat"
+          style={{ backgroundImage: `url(${bg})` }}
+        />
+        <Nav />
+
+      </section>
+      <main className="h-500" />
     </div>
   )
 }
--- frontend/src/components/nav.tsx
+++ frontend/src/components/nav.tsx
@@ -1,37 +1,30 @@
-import { useState } from "react"
-
 const items = [
   { text: "Philosophy", link: "Philosophy" },
   { text: "Data", link: "Data" },
   { text: "Contact", link: "Contact" },
 ]
 
-
 export default function Nav() {
-
   return (
-    <div className={` w-full max-w-225 p-5 z-50`}>
-      <section className="bg-black text-white p-5 flex flex-row justify-between items-center border-4 rounded-3xl tracking-tighter">
-        <a
-          href="/"
-          className={`text-2xl font-black rounded-2xl hover:text-blue-500 transition-all`}
-        >
-          Stirling's!
-        </a>
+    <div className="z-10 w-full px-5 py-2 text-white sticky top-0">
+      <nav className="flex flex-row items-baseline justify-between">
+        <div className="text-2xl font-semibold tracking-tighter">
+          stirling[s].
+        </div>
 
+        <div className="w-20">
+          <div className="h-[1.5px]  flex-1 bg-white/80" />
+          <div className="h-[1.5px] -translate-y-2 flex-1 bg-white/80" />
 
-        <section className="flex flex-row  gap-5">
+        </div>
+        <div className="text-sm flex flex-row gap-5 uppercase font-semibold tracking-tighter ">
           {items.map((item, index) => (
-            <a
-              key={index}
-              href={`/${item.link}`}
-              className="hover:scale-105 origin-center rounded-xl hover:bg-white hover:text-black p-2 transition-all"
-            >
+            <div key={index}>
               {item.text}
-            </a>
+            </div>
           ))}
-        </section>
-      </section>
+        </div>
+      </nav>
     </div>
   )
 }
